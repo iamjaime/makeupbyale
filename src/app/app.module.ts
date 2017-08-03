2e8e91cc21e95d7da058ee7b090def628fe991d7
CodeHead <iamjaime89@gmail.com>
2017-08-02 20:40:45 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path : 'home',
+        component : HomeComponent
+      },
+      {
+        path : 'about',
+        component : AboutComponent
+      },
+      {
+        path : 'services',
+        component : ServicesComponent
+      },
+      {
+        path : 'portfolio',
+        component : PortfolioComponent
+      },
+      {
+        path : 'contact',
+        component : ContactComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
